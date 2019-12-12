e8441b91b231982ea59be372c1fe52e816214474
Saara Koskipaa <saara.koskipaa@iki.fi>
2019-12-12 19:54:00 +0200

    Blogilista 4.20*, 4.21*

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -24,10 +24,14 @@ mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
 
 app.use(cors())
 app.use(bodyParser.json())
+
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
         .find({})
@@ -20,15 +12,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-
-    const token = getTokenFrom(request)
+    
     try {
-        const decodedToken = jwt.verify(token, process.env.SECRET)
-        if (!token || !decodedToken.id) {
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        if (!request.token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
     
-
         const user = await User.findById(decodedToken.id)
         const blog = new Blog({
             title: body.title,
@@ -50,13 +40,41 @@ blogsRouter.post('/', async (request, response, next) => {
 
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+/*blogsRouter.delete('/:id', async (request, response, next) => {
     try {
         await Blog.findByIdAndRemove(request.params.id)
         response.status(204).end()
     } catch (exception) {
         next(exception)
     }
+})*/
+
+blogsRouter.delete('/:id', async (request, response, next) => {        
+        try {
+            const blogToDelete = await Blog.findById(request.params.id)
+            const decodedToken = jwt.verify(request.token, process.env.SECRET)
+            
+            if (!request.token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+            }
+
+            const user = await User.findById(decodedToken.id)
+            
+            if (blogToDelete.user.toString() !== user._id.toString()) {
+                return response.status(403).json({ error: 'Not allowed' })
+            }
+
+            try {
+                await Blog.findByIdAndRemove(request.params.id)
+                response.status(204).end()
+            } catch (exception) {
+                next(exception)
+            }
+
+        } catch (exception) {
+            next(exception)
+        }
+    
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
